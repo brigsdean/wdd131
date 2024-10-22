89acea358e7c87da76f71bcd0ed233206d965c2c
Brigham Dean <Briggs-dean@outlook.com>
2024-10-22 10:57:25 -0800

    clrfix2

--- projects/scripts/temple.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuIcon = document.querySelector('.menu-icon');
-    const navMenu = document.querySelector('header nav ul');
-
-    menuIcon.addEventListener('click', () => {
-        navMenu.style.display = navMenu.style.display === 'block' ? 'none' : 'block';
-    });
-});
-
